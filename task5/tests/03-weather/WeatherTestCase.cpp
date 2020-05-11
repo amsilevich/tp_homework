246676c980054b39e52bed184e014aa0c663b2ce
Artem <amsilevich@gmail.com>
2020-05-11 03:07:59 +0300

    3:07

--- task5/tests/03-weather/WeatherTestCase.cpp
+++ task5/tests/03-weather/WeatherTestCase.cpp
@@ -12,7 +12,7 @@ void WeatherTestCase::SetUp() {
     city1.text = "{\"cod\":\"200\",\"message\":0.0036,\"cnt\":40,\"list\":[{\"dt\":1485799200,\"main\":{\"temp\":15.00}}]}";
 
     city2.status_code = 200;
-    city2.text = {\"cod\":\"200\",\"message\":0.0036,\"cnt\":40,\"list\":[{\"dt\":1485799200,\"main\":{\"temp\":20.00}}]}";;
+    city2.text = "{\"cod\":\"200\",\"message\":0.0036,\"cnt\":40,\"list\":[{\"dt\":1485799200,\"main\":{\"temp\":20.00}}]}";
 
     error_401.status_code = 401;
 }
@@ -48,7 +48,7 @@ TEST_F(WeatherTestCase, test4) {
         .Times(1)
             .WillRepeatedly(Return(city2));
     
-    ASSERT_EQ(weather.GetDifferenceString("London", "Moscow"), "Weather in London is warmer than in Moscow by 5 degrees");
+    ASSERT_EQ(weather.GetDifferenceString("London", "Moscow"), "Weather in London is colder than in Moscow by 5 degrees");
 }
 
 TEST_F(WeatherTestCase, test5) {
